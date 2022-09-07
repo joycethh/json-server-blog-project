657c6081a8928aba4ebf37e0225599f07060cd31
joycethh <joycethh@outlook.com>
2022-09-07 17:07:06 -0500

    use json server to mock rest json server

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { blogsData } from "./blogsData";
+
 import { BlogList } from "./BlogList";
 
 export const Home = () => {
-  const [blogs, setBlogs] = useState(blogsData);
+  const [blogs, setBlogs] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -11,12 +12,27 @@ export const Home = () => {
   };
 
   useEffect(() => {
-    console.log("useEffect got fired");
-    console.log("blogs-state", blogs);
-  });
+    fetch("http://localhost:8000/blogs")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+        setisLoading(false);
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {isLoading ? (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      ) : (
+        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      )}
+      {/* {blogs && (
+        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      )} */}
     </div>
   );
 };
