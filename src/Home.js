1e049967446a53f7ae772883ae72615f1ae4de0a
joycethh <joycethh@outlook.com>
2022-09-07 14:51:51 -0500

    create blogsData and use it in home page

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { blogsData } from "./blogsData";
 
 export const Home = () => {
-  return <div>Home</div>;
+  const [blogs, setBlogs] = useState(blogsData);
+  return (
+    <div className="home">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
---
+++ src/blogsData.js
@@ -0,0 +1,18 @@
+export const blogsData = [
+  {
+    title: "Tiger-slayer Wu Song",
+    body: "Wu Song is a legendary figure from fiction the Water Margin. He is famous for slying tiger with barehands, avenging his brother and then finally become an outlaw.",
+    author: "Joyce",
+    id: 1,
+  },
+  {
+    title: "Tatoo Monk Lu Zhishen",
+    body: "Lu Zhishen is also a lengendary hero from fiction the Water Margin. ",
+    author: "Joyce",
+    id: 2,
+  },
+  {
+    title: "the Water Margin Characters",
+    body: "The 108 Stars of Destiny are at the core of the plot of Water Margin. It inclues 36 Heaveenly stars and 72 Earthly Fiends",
+  },
+];
