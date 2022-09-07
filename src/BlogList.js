d4676039449c9fc93ef96e836f34d23dc7a70574
joycethh <joycethh@outlook.com>
2022-09-07 15:10:30 -0500

    seperate the blogList as individual components

---
+++ src/BlogList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const BlogList = ({ blogs }) => {
+  return (
+    <div className="blog-list">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import { blogsData } from "./blogsData";
+import { BlogList } from "./BlogList";
 
 export const Home = () => {
   const [blogs, setBlogs] = useState(blogsData);
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} key={blogs.id} />
     </div>
   );
 };
