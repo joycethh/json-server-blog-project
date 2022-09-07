5f1800a073dbc3cbd515044a65548961abb3720c
joycethh <joycethh@outlook.com>
2022-09-07 13:40:52 -0500

    create home and navbar components

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The Book Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  );
+};
