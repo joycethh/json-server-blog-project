5f908de01453a1e29e313efcc180c26c6fb3218f
joycethh <joycethh@outlook.com>
2022-09-08 11:30:01 -0500

    install react-router-dom and create new create route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./Navbar";
 import { Home } from "./Home";
+import { Create } from "./Create";
+
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/create" element={<Create />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
