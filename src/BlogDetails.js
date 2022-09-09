fe922900ea8fbc8c02d0b4a72a92ad11d6ab5975
joycethh <joycethh@outlook.com>
2022-09-09 12:35:43 -0500

    add delete function

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 import { useFetch } from "./useFetch";
 
 export const BlogDetails = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const {
     data: blog,
     isLoading,
     error,
   } = useFetch(`http://localhost:8000/blogs/${id}`);
 
+  const handleDelete = () => {
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: "DELETE",
+    }).then(() => navigate("/", { replace: true }));
+  };
   return (
     <div className="blog-details">
       {error && (
@@ -29,6 +35,7 @@ export const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Author: {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
