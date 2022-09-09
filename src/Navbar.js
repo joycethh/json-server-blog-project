0fce97ed9036e4d7c538c33feecdac35157a8bb9
joycethh <joycethh@outlook.com>
2022-09-09 13:46:57 -0500

    update name

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Book Blog</h1>
+      <h1>The Blog</h1>
       <div className="links">
         <Link to="/">Home</Link>
-        <Link to="/create">New Blog</Link>
+        <Link to="/create">Create New Blog</Link>
       </div>
     </nav>
   );
