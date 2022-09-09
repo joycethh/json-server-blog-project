971c04d89cdcb22b26df10b9a86fcb5410a1e882
joycethh <joycethh@outlook.com>
2022-09-08 22:57:31 -0500

    create CreateArea

--- src/Create.js
+++ src/Create.js
@@ -1,5 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Create = () => {
-  return <div>Create CreateCreateCreate</div>;
+  const [input, setInput] = useState({
+    title: "",
+    body: " ",
+    author: " ",
+  });
+
+  const handleChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBlog = input;
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newBlog),
+    }).then(() => console.log("post request", fetch()));
+  };
+
+  return (
+    <div className="create">
+      <h2> Add a New Blog</h2>
+      <form>
+        <label>Title: </label>
+        <input
+          name="title"
+          type="text"
+          value={input.title}
+          onChange={handleChange}
+        />
+        <label>Content:</label>
+        <textarea
+          name="body"
+          type="text"
+          value={input.body}
+          onChange={handleChange}
+        />
+        <label>Author</label>
+        <select>
+          <option value={input.author}>{input.author}</option>
+        </select>
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+    </div>
+  );
 };
